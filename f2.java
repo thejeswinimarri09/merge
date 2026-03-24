3e00b0849a6d49bdcc320f5ad058429c677a5175
thejeswinimarri <marrithejeswini@gmail.com>
2026-03-23 19:08:20 -0700

    added f2.java in master

---
+++ f2.java
@@ -0,0 +1,9 @@
+class People{
+    public static void main(String[] args)
+    {
+        int age = 20;
+        String name = "Teju";
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
