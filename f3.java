bd8643e28428eaee8eab82ef480570e3cd3267e9
thejeswinimarri <marrithejeswini@gmail.com>
2026-03-23 19:08:33 -0700

    added f3.java in master

---
+++ f3.java
@@ -0,0 +1,11 @@
+class Player{
+    int num1 = 18;
+    String name = "Naggi"
+    public static void main(String[] args)
+    {
+        Player p1 = new Player();
+        System.out.println("Player num : " + num1);
+        System.out.println("Player name: " + name);
+
+    }
+}
